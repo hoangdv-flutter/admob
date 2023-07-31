ba78c290916f2a37d8a97163699990626f968209
Hoangkute123xyz <hoangdv@wavez.vn>
2023-07-31 17:41:32 +0700

    update

--- lib/ads_loader.dart
+++ lib/ads_loader.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:admob/app_open/app_open_ads_loader.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:injectable/injectable.dart';
 
@@ -7,15 +8,21 @@ import 'package:injectable/injectable.dart';
 class AdsLoader {
   var isInitial = false;
 
-  AdsLoader() {
+  final AppOpenAdsLoader _appOpenAdsLoader;
+
+  AdsLoader(this._appOpenAdsLoader) {
     _init();
   }
 
   Future<void> _init() async {
     MobileAds.instance.updateRequestConfiguration(RequestConfiguration(
-        testDeviceIds: ['2F4D0760CC76965FA11B704EB25F30A7','987E51C0FF974F2007128FBAAAAA201A']));
+        testDeviceIds: [
+          '2F4D0760CC76965FA11B704EB25F30A7',
+          '987E51C0FF974F2007128FBAAAAA201A'
+        ]));
     await MobileAds.instance.initialize().then((value) {
       isInitial = true;
+      _appOpenAdsLoader.loadAd();
     }, onError: (e) {
       isInitial = false;
       return _init();
