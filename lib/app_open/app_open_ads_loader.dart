7dc308abc7bd25fc7e14b0469c98656067b98113
trungtang <trungtv@wazez.vn>
2025-06-03 13:12:25 +0700

    update

--- lib/app_open/app_open_ads_loader.dart
+++ lib/app_open/app_open_ads_loader.dart
@@ -7,7 +7,6 @@ import 'package:flutter_core/data/shared/premium_holder.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:injectable/injectable.dart';
 import 'package:rxdart/rxdart.dart';
-import 'package:synchronized/synchronized.dart';
 
 @singleton
 class AppOpenAdsLoader {
@@ -23,22 +22,16 @@ class AppOpenAdsLoader {
 
   ValueStream<bool> get adShowingState => _adShowingState.stream;
 
-  final _loadOpenAdsAppState = BehaviorSubject<DataState>();
-
-  ValueStream<DataState> get loadOpenAppAdsState => _loadOpenAdsAppState.stream;
-
   static var isShowing = false;
 
   var _busy = false;
 
   bool get availableAd => _availableAd != null;
 
-  final lock = Lock(reentrant: true);
-
   Future<void> show({Function()? onShowed}) async {
     if (_premiumHolder.isPremium) return;
     if (_availableAd == null) {
-      // loadAd();
+      loadAd();
       return;
     }
     final adShared = appInject<AdShared>();
@@ -48,13 +41,9 @@ class AppOpenAdsLoader {
         onAdShowedFullScreenContent: (ad) {
           isShowing = true;
           _adShowingState.addSafety(true);
-          adShared.lastTimeLoadAds = DateTime
-              .now()
-              .millisecondsSinceEpoch;
+          adShared.lastTimeLoadAds = DateTime.now().millisecondsSinceEpoch;
           adShared.lastTimeShowAppOpenAds =
-              DateTime
-                  .now()
-                  .millisecondsSinceEpoch;
+              DateTime.now().millisecondsSinceEpoch;
         },
         onAdFailedToShowFullScreenContent: (ad, error) {
           isShowing = false;
@@ -68,9 +57,7 @@ class AppOpenAdsLoader {
           isShowing = false;
           _adShowingState.addSafety(false);
           adShared.lastTimeShowAppOpenAds =
-              DateTime
-                  .now()
-                  .millisecondsSinceEpoch;
+              DateTime.now().millisecondsSinceEpoch;
           onShowed?.call();
           _availableAd = null;
         },
@@ -79,21 +66,9 @@ class AppOpenAdsLoader {
   }
 
   Future<void> loadAd() async {
-    _loadOpenAdsAppState.addSafety(DataState.loading);
-    await Future.delayed(Duration(milliseconds: 1000));
-    if (_premiumHolder.isPremium) {
-      _loadOpenAdsAppState.addSafety(DataState.error);
-      return ;
-    }
-    if (!appInject<AdsLoader>().isInitial) {
-      _loadOpenAdsAppState.addSafety(DataState.error);
-      return ;
-    }
-    if (_busy || availableAd) {
-      _availableAd = null;
-      _loadOpenAdsAppState.addSafety(DataState.error);
-      return;
-    }
+    if (_premiumHolder.isPremium) return;
+    if (!appInject<AdsLoader>().isInitial) return;
+    if (_busy || availableAd) return;
     _busy = true;
     _availableAd = null;
     await AppOpenAd.load(
@@ -103,11 +78,9 @@ class AppOpenAdsLoader {
           onAdLoaded: (ad) {
             _availableAd = ad;
             ad.onPaidEvent = GlobalAdListener.onPaidEventCallback;
-            _loadOpenAdsAppState.addSafety(DataState.loaded);
           },
           onAdFailedToLoad: (error) {
             print("Open app ads loaded failed ${error.message}");
-            _loadOpenAdsAppState.addSafety(DataState.error);
           },
         ));
     _busy = false;
@@ -121,7 +94,6 @@ class AppOpenAdsLoader {
   void _clearAds() {
     _adShowingState.close();
     _availableAd?.dispose();
-    _loadOpenAdsAppState.close();
     _availableAd = null;
   }
 }
